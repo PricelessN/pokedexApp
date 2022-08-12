120956169c066dccf990524fb035c39774d1621f
PricelessN <110741954+PricelessN@users.noreply.github.com>
2022-08-12 15:04:25 +0100

    Add files via upload

---
+++ pages/priceless.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function Priceless() {
+  return <div>
+    <p>Priceless is cool</p>
+    <Link href="/">
+        <a>Back</a>
+    </Link>
+  </div>
+}
